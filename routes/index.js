aeb9aca12b229f18ccfe16ff9643a64ccffe201f
simzeee <johnsims.dev@gmail.com>
2025-06-30 15:54:43 -0400

    add directory sync and SSO at the MVP level

--- routes/index.js
+++ routes/index.js
@@ -1,99 +1,138 @@
-import express from 'express'
-import session from 'express-session'
-import { WorkOS } from '@workos-inc/node'
+import express from "express";
+import session from "express-session";
+import { WorkOS } from "@workos-inc/node";
 
-const app = express()
-const router = express.Router()
+const app = express();
+const router = express.Router();
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+const clientID = process.env.WORKOS_CLIENT_ID;
+const organizationID = process.env.ORGANIZATION_ID;
+const redirectURI = "http://localhost:8000/callback";
+const state = "";
+
+// // Fetch all Directory Users in a Directory
+// const usersFromDirectory = await workos.directorySync.listUsers({
+//   directory: process.env.DIRECTORY_ID,
+// });
+
+// // Fetch all Directory Users in a Directory Group
+// const usersByGroup = await workos.directorySync.listUsers({
+//   group: "directory_group_01JZ156DN2FZ64A9DNJVEGZ5Y8",
+// });
+
+// console.log("here", usersFromDirectory.data);
+
+// console.log(usersByGroup.data);
 
 app.use(
-    session({
-        secret: 'keyboard cat',
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: true },
-    })
-)
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 
-const workos = new WorkOS(process.env.WORKOS_API_KEY)
-const clientID = process.env.WORKOS_CLIENT_ID
-const organizationID = 'org_test_idp'
-const redirectURI = 'http://localhost:8000/callback'
-const state = ''
+router.get("/", function (req, res) {
+  if (session.isloggedin) {
+    res.render("login_successful.ejs", {
+      profile: session.profile,
+      first_name: session.first_name,
+    });
+  } else {
+    res.render("index.ejs", { title: "Home" });
+  }
+});
 
-router.get('/', function (req, res) {
-    if (session.isloggedin) {
-        res.render('login_successful.ejs', {
-            profile: session.profile,
-            first_name: session.first_name,
-        })
-    } else {
-        res.render('index.ejs', { title: 'Home' })
-    }
-})
-
-router.post('/login', (req, res) => {
-    const login_type = req.body.login_method
-
-    const params = {
-        clientID: clientID,
-        redirectURI: redirectURI,
-        state: state,
-    }
-
-    if (login_type === 'saml') {
-        params.organization = organizationID
-    } else {
-        params.provider = login_type
-    }
-
+router.get("/directory", async (req, res, next) => {
     try {
-        const url = workos.sso.getAuthorizationURL(params)
+    // fetch *all* users in the directory
+    const { data: users } = await workos.directorySync.listUsers({
+      directory: process.env.DIRECTORY_ID,
+    });
 
-        res.redirect(url)
-    } catch (error) {
-        res.render('error.ejs', { error: error })
-    }
+    // render a view called "directory.ejs" and pass the users array
+    console.log("users", users)
+    res.render("directory.ejs", { users });
+  } catch (error) {
+    next(error);
+  }
 })
 
-router.get('/callback', async (req, res) => {
-    let errorMessage
-    try {
-        const { code, error } = req.query
+router.post("/login", (req, res) => {
+  const login_type = req.body.login_method;
+  console.log("login type", login_type);
 
-        if (error) {
-            errorMessage = `Redirect callback error: ${error}`
-        } else {
-            const profile = await workos.sso.getProfileAndToken({
-                code,
-                clientID,
-            })
-            const json_profile = JSON.stringify(profile, null, 4)
+  const params = {
+    clientID: clientID,
+    redirectURI: redirectURI,
+    organization: process.env.ORGANIZATION_ID,
+    state: state,
+  };
 
-            session.first_name = profile.profile.first_name
-            session.profile = json_profile
-            session.isloggedin = true
-        }
-    } catch (error) {
-        errorMessage = `Error exchanging code for profile: ${error}`
-    }
+  if (login_type === "saml") {
+    params.organization = organizationID;
+  } else {
+    params.provider = login_type;
+  }
 
-    if (errorMessage) {
-        res.render('error.ejs', { error: errorMessage })
+  try {
+    const url = workos.sso.getAuthorizationURL(params);
+
+    res.redirect(url);
+  } catch (error) {
+    res.render("error.ejs", { error: error });
+  }
+});
+
+router.get("/callback", async (req, res) => {
+  let errorMessage;
+  try {
+    const { code, error } = req.query;
+
+    if (error) {
+      errorMessage = `Redirect callback error: ${error}`;
     } else {
-        res.redirect('/')
+      const profile = await workos.sso.getProfileAndToken({
+        code,
+        clientID,
+      });
+      const json_profile = JSON.stringify(profile, null, 4);
+
+      const checkID = "org_01JZ0V6JQEKWNRMGJF2F52704G";
+      console.log(checkID, profile.profile);
+      //   console.log(json_profile);
+      if (profile.profile.organization_id !== checkID) {
+        return res.status(401).send({
+          message: "Unauthorized",
+        });
+      }
+
+      session.first_name = profile.profile.first_name;
+      session.profile = json_profile;
+      session.isloggedin = true;
     }
-})
+  } catch (error) {
+    errorMessage = `Error exchanging code for profile: ${error}`;
+  }
 
-router.get('/logout', async (req, res) => {
-    try {
-        session.first_name = null
-        session.profile = null
-        session.isloggedin = null
+  if (errorMessage) {
+    res.render("error.ejs", { error: errorMessage });
+  } else {
+    res.redirect("/");
+  }
+});
 
-        res.redirect('/')
-    } catch (error) {
-        res.render('error.ejs', { error: error })
-    }
-})
+router.get("/logout", async (req, res) => {
+  try {
+    session.first_name = null;
+    session.profile = null;
+    session.isloggedin = null;
 
-export default router
+    res.redirect("/");
+  } catch (error) {
+    res.render("error.ejs", { error: error });
+  }
+});
+
+export default router;
